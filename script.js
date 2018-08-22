bae2a74fe97ddeee66eb928e493cdde56b7f6099
Janny Huang <jannyhuang12@gmail.com>
2018-08-21 20:59:46 -0400

    add files

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("navMenu").innerHTML =
+ 	"<div class=\"banner\">" +
+		"<nav style=\"width: max-content;\">" + 
+		"<a href=\"home.html\">Home</a>"+
+			"<div class=\"drop\">" +
+				"<button class=\"dropbtn\">Menu</button>" +
+				"<div class=\"dropdown\">" +
+					"<a href=\"prof.html\">About Me</a>" +
+					"<a href=\"portfolio.html\">Portfolio</a>" +
+					"<a href=\"contact.html\">Contact</a>" +
+				"</div>" +
+			"</div>" +
+		"</nav>" +
+	"</div>";
---
+++ script2.js
@@ -0,0 +1,98 @@
+function showAlert(){
+	alert("Hello World!");
+}
+
+function navBar(){
+	document.getElementById("navMenu").innerHTML =
+ 	"<div class=\"banner\">" +
+		"<nav style=\"width: max-content;\">" + 
+		"<a href=\"home.html\">Home</a>"+
+			"<div class=\"drop\">" +
+				"<button class=\"dropbtn\">Menu</button>" +
+				"<div class=\"dropdown\">" +
+					"<a href=\"prof.html\">About Me</a>" +
+					"<a href=\"portfolio.html\">Portfolio</a>" +
+					"<a href=\"contact.html\">Contact</a>" +
+				"</div>" +
+			"</div>" +
+		"</nav>" +
+	"</div>";
+
+}
+
+function displayClock(){
+	var clock = document.getElementById('clock');
+	var now = new Date();
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+	var seconds = now.getSeconds();
+
+	if (minutes < 10){
+		minutes = "0" + minutes;
+	}
+	if(seconds < 10){
+		seconds = "0" + seconds;
+	}
+	if(hours > 12 && hours < 24){
+		var timeOfDay = "pm";
+		hours -= 12;
+	}
+	else{
+		var timeOfDay = "am";
+	}
+
+	var currentTime = hours + ":" + minutes + ":" + seconds + timeOfDay;
+
+	clock.innerHTML = currentTime;
+}
+
+function toggleClock(){
+	var clock = document.getElementById("clock");
+	var display = clock.style.display;
+	var clockbtn = document.getElementById("clockbtn");
+
+	if (display == "block"){
+		clock.style.display = "none";
+		clockbtn.innerHTML = "Show clock";
+	}
+	else{
+		clock.style.display = "block";
+		clockbtn.innerHTML = "Hide clock";
+	}
+}
+
+/*function enlargeText(){
+	var paragraph = document.getElementsByTagName('p');
+	paragraph[1].style.fontSize="36px";
+}*/
+
+
+function setCanvas(){
+	/* creates canvas for interactive! */
+	/* creates drawing object for canvas */
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle = "red";
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+	canvas.addEventListener("mousemove", function myFunction(){
+		canvas.onmousemove = function(e){
+		var r = Math.floor(Math.random() * 255 + 1), 
+		    g = Math.floor(Math.random() * 255 + 1), 
+		    b = Math.floor(Math.random() * 255 + 1);
+		var color = "rgb(" + r + "," + g + "," + b + ")";
+		ctx.fillStyle = color;
+		var x = e.clientX - canvas.width;
+		var y = e.clientY - canvas.height;
+		ctx.beginPath();
+		ctx.arc(x, y, Math.floor(Math.random() * 30 + 1), 0, 2 * Math.PI);
+		ctx.stroke();
+		ctx.fill();
+		}
+
+	});
+
+}
+
+$(".parent").on("click", function(){
+	$(this).toggleClass("trash"); //click will transform or bring it back
+});
